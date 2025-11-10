5ea9f1c715d8efbcb790046f1b9b8d9b5409574c
hardiksharma04 <hardiksharma7004@gmail.com>
2025-11-10 10:16:23 +0530

    Initial backend commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import mysql.connector
+from db_config import get_connection
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Portfolio Backend Running ✅"})
+
+@app.route('/add_project', methods=['POST'])
+def add_project():
+    data = request.get_json()
+    title = data['title']
+    description = data['description']
+    link = data['link']
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO projects (title, description, link) VALUES (%s, %s, %s)", (title, description, link))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Project Added ✅"})
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM projects")
+    projects = cursor.fetchall()
+    conn.close()
+    return jsonify(projects)
+
+if __name__ == '__main__':
+    app.run(debug=True)
